6f3587993ba6fd7dc58cd6b0735d79c090a5b023
Maeve Thompson <maevet78@gmail.com>
2019-07-11 15:03:41 -0500

    Initial Commit

---
+++ Emojinal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Emojinal
+//
+//  Created by Maeve Thompson on 7/11/19.
+//  Copyright © 2019 Maeve Thompson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
