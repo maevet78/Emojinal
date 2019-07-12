a09921120685dbaccb1f42cbef6c3281450d5361
Maeve Thompson <maevet78@gmail.com>
2019-07-12 15:36:01 -0500

    Completion Commit

--- Emojinal/ViewController.swift
+++ Emojinal/ViewController.swift
@@ -10,76 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    let emojis = ["👩‍💻": "Called the Queen of Software, she helped invent some of the early English-language programming languages.", "🧮": "She developed an algorithm for a computer that didn’t yet exist — an accomplishment that some say qualifies her as the world’s first computer programmer.", "☎️": "She invented a telephony switching computer program that kept phones functioning under stressful loads.", "🗺️": "She is Google’s first female engineer (she’s actually employee number 20). Now vice president of location and local services, Mayer leads product management and engineering for a variety of search products, including Google Maps, Local Search, Google Earth, Street View and Latitude.", "📻": "She played a key role in the invention of spread-spectrum technology; specifically, by conceptualizing the idea of frequency hopping, which is a method of sending radio signals from different frequency channels.", "🎮": "Sierra On-Line was the name of company (later known as Sierra Entertainment) that Williams co-founded with her husband, Ken Williams. Together, they helped shape the history of video games with their complex puzzles and detailed storylines."]
-    
-    @IBAction func showMessage(_ sender: UIButton){
-      let selectedEmotion = sender.titleLabel?.text
-        
-        
-        
-    }
-   
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-    @IBAction func graceHopper(_ sender: UIButton) {
-        let alertController = UIAlertController(title: "Grace Hopper (Programming Pioneer)", message: "Called the Queen of Software, she helped invent some of the early English-language programming languages.", preferredStyle: UIAlertController.Style.alert)
-        
-         alertController.addAction(UIAlertAction(title: "♀️K", style: UIAlertAction.Style.default, handler: nil))
-        
-        present(alertController, animated: true, completion: nil)
-        
-    }
-    
-    @IBAction func adaLovelace(_ sender: UIButton) {
-         let alertController = UIAlertController(title: "Ada Lovelace (Algorithm Enchantress)", message: "She developed an algorithm for a computer that didn’t yet exist — an accomplishment that some say qualifies her as the world’s first computer programmer.", preferredStyle: UIAlertController.Style.alert)
-        
-         alertController.addAction(UIAlertAction(title: "♀️K", style: UIAlertAction.Style.default, handler: nil))
-        
-        present(alertController, animated: true, completion: nil)
-        
-    }
     
     
-    @IBAction func ernaHoover(_ sender: UIButton) {
-        let alertController = UIAlertController(title: "Erna Hoover (Telephony Hero)", message: "She invented a telephony switching computer program that kept phones functioning under stressful loads.", preferredStyle: UIAlertController.Style.alert)
-        
-         alertController.addAction(UIAlertAction(title: "♀️K", style: UIAlertAction.Style.default, handler: nil))
-        
-        present(alertController, animated: true, completion: nil)
-        
-    }
+    let emojis = ["👩‍💻": "Grace Hopper", "🧮": "Ada Lovelace", "☎️": "Dr. Erna Hoover", "🗺️": "Marissa Mayer", "📻": "Hedy Lamar", "🎮": "Roberta Williams"]
     
-    @IBAction func marissaMayer(_ sender: UIButton) {
-         let alertController = UIAlertController(title: "Marissa Mayer (Search Siren)", message: "She is Google’s first female engineer (she’s actually employee number 20). Now vice president of location and local services, Mayer leads product management and engineering for a variety of search products, including Google Maps, Local Search, Google Earth, Street View and Latitude.", preferredStyle: UIAlertController.Style.alert)
-        
-         alertController.addAction(UIAlertAction(title: "♀️K", style: UIAlertAction.Style.default, handler: nil))
-        
-        present(alertController, animated: true, completion: nil)
-        
-    }
+   let customMessages = ["Grace Hopper": ["Called the Queen of Software, she helped invent some of the early English-language programming languages.", "Programming Pioneer", "Grace Hopper Celebration is the world's largest gathering of women technologists."], "Ada Lovelace": ["She developed an algorithm for a computer that didn’t yet exist — an accomplishment that some say qualifies her as the world’s first computer programmer.", "Algorithm Enchantress", "She has been referred to as 'prophet of the computer age'. Certainly she was the first to express the potential for computers outside mathematics."], "Dr. Erna Hoover": ["She invented a telephony switching computer program that kept phones functioning under stressful loads.", "Telephony Hero", "Hoover's system earned her a position as the first female supervisor of a technical department at Bell Labs. The principles of Hoover's switching system are still widely used today, as various communications companies struggle with ever-increasing incoming traffic."], "Marissa Mayer": ["She is Google’s first female engineer (she’s actually employee number 20). Now vice president of location and local services, Mayer leads product management and engineering for a variety of search products, including Google Maps, Local Search, Google Earth, Street View and Latitude.", "Search Siren", "In 2012 Mayer became chief executive officer and president of Yahoo! Inc., a global Internet-services company and one of Google’s largest competitors"], "Hedy Lamar": ["She played a key role in the invention of spread-spectrum technology; specifically, by conceptualizing the idea of frequency hopping, which is a method of sending radio signals from different frequency channels.", " Wireless Visionary", "Originally designed to defeat the German Nazis, the system became an important step in the development of technology to maintain the security of both military communications and cellular phones."], "Roberta Williams": ["Sierra On-Line was the name of company (later known as Sierra Entertainment) that Williams co-founded with her husband, Ken Williams. Together, they helped shape the history of video games with their complex puzzles and detailed storylines.", "Gaming Genius", "'In the old days, when I first started designing adventure games, there wasn't much plot. You kind of ran around beating up trolls and gathering treasure.'"] ]
     
-    @IBAction func hedyLamarr(_ sender: UIButton) {
-        let alertController = UIAlertController(title: "Hedy Lamarr (Wireless Visionary)", message: "She played a key role in the invention of spread-spectrum technology; specifically, by conceptualizing the idea of frequency hopping, which is a method of sending radio signals from different frequency channels.", preferredStyle: UIAlertController.Style.alert)
+    @IBAction func showMessage(_ sender:UIButton)
+    {
+        let selectedEmotion = sender.titleLabel?.text
+        
+        let random:UInt32 = arc4random_uniform(3)
+        
+        let emojiMessage = customMessages[emojis[selectedEmotion!]!]?[Int(random)]
+        
+        let alertController = UIAlertController(title: emojis[selectedEmotion!], message: emojiMessage, preferredStyle: UIAlertController.Style.alert)
         
         alertController.addAction(UIAlertAction(title: "♀️K", style: UIAlertAction.Style.default, handler: nil))
         
         present(alertController, animated: true, completion: nil)
-        
     }
- 
-    @IBAction func robertaWilliams(_ sender: UIButton) {
-        let alertController = UIAlertController(title: "Roberta Williams (Gaming Genius)", message: "Sierra On-Line was the name of company (later known as Sierra Entertainment) that Williams co-founded with her husband, Ken Williams. Together, they helped shape the history of video games with their complex puzzles and detailed storylines.", preferredStyle: UIAlertController.Style.alert)
-        
-        alertController.addAction(UIAlertAction(title: "♀️K", style: UIAlertAction.Style.default, handler: nil))
-        
-        present(alertController, animated: true, completion: nil)
-        
-        
-    }
-    
 }
-
